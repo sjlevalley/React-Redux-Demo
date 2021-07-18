584473f31ecd1ec2783ffde384b82e6955c15c8d
Steve LeValley <slevalley@gmail.com>
2021-07-18 14:07:49 -0700

    Updates

---
+++ precache-manifest.61d81b1f36e185ccf3d5658387fe9bc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e3f66a40106243340a5072ceb5e0b51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "234bd36ea2af7e4edc0d",
+    "url": "/static/css/main.87992b5b.chunk.css"
+  },
+  {
+    "revision": "a13626790270a60eecf4",
+    "url": "/static/js/2.afa363b6.chunk.js"
+  },
+  {
+    "revision": "b90d08aa382f855adb42c66cf542ea81",
+    "url": "/static/js/2.afa363b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "234bd36ea2af7e4edc0d",
+    "url": "/static/js/main.ddd43ed1.chunk.js"
+  },
+  {
+    "revision": "5d36e8b979177fafb9d7",
+    "url": "/static/js/runtime-main.0be38ffb.js"
+  }
+]);
